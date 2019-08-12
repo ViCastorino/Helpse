fa387aa2d1df6e899d9fb2cbcfff40a4189c854e
BabiMoreira <babisantos631@gmail.com>
2019-08-12 12:12:45 -0300

    adicionar carossel

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -25,4 +25,34 @@
 // });
 
 
+// função do carrosel ///
+
+let slideIndex = 1;
+slideShow(slideIndex);
+
+
+function plusSlides(n) { slideShow(slideIndex += n); }
+
+function currentSlide(n) { slideShow(slideIndex = n); }
+
+function slideShow(n) {
+    let i;
+    let slides = document.getElementsByClassName("text-video");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activate", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+}
+
+
 
