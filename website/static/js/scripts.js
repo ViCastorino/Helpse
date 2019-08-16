aaecf60e071d4be5a0dc3796ea33fb226ff53c00
BabiMoreira <babisantos631@gmail.com>
2019-08-16 00:24:24 -0300

    remover função do carrosel

--- website/static/js/scripts.js
+++ website/static/js/scripts.js
@@ -25,33 +25,3 @@ btnorganization.addEventListener('click', function(){
 });
 
 // função do carrosel ///
-
-let slideIndex = 1;
-slideShow(slideIndex);
-
-
-function plusSlides(n) { slideShow(slideIndex += n); }
-
-function currentSlide(n) { slideShow(slideIndex = n); }
-
-function slideShow(n) {
-    let i;
-    let slides = document.getElementsByClassName("text-box");
-    // let dots = document.getElementsByClassName("bar");
-
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" activate", "");
-    // }
-    slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex - 1].className += " active";
-
-}
-
-
-
